dafdcc6da6758ab49dc11a6dbd3e66a5ffd03d67
Silpa <silpa.jr@gmail.com>
2024-08-25 10:03:59 +0800

    array variables

---
+++ arrays.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# array index starts with 0 size is 3
+FRUITS=("Apple" "Banana" "Mango")
+echo "first fruit is ${FRUITS[0]}"
+echo "second fruit is ${FRUITS[1]}"
+echo "third fruit is ${FRUITS[2]}"
