bac926d397b4c388e85a8279b0ca105bea5da097
Silpa <silpa.jr@gmail.com>
2024-08-25 09:04:04 +0800

    variables commit

---
+++ consversation.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+echo "Ramesh: Hello Suresh"
+echo "Suresh: I am fine Ramesh, how are you?"
+echo "Ramesh: I sm fine Suresh, what's going on" 
+echo "Suresh:  am learning devops Ramesh, Thank you"
---
+++ variables1.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+PERSON1="Ramesh"
+PERSON2="Suresh"
+echo "$PERSON1: Hello ${PERSON2}"
+echo "$PERSON2: I am fine Ramesh, how are you?"
+echo "$PERSON1: I sm fine ${PERSON2}, what's going on" 
+echo "${PERSON1}:  am learning devops $PERSON2, Thank you"
