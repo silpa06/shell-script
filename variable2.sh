cbcf3df8a206d7f316a6908248e3ade1d6594510
Silpa <silpa.jr@gmail.com>
2024-08-25 09:43:58 +0800

    arg variables

---
+++ variable2.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: Hello ${PERSON2}"
+echo "${PERSON2}: I am fine $PERSON1, how are you?"
+echo "$PERSON1: I sm fine ${PERSON2}, what's going on" 
+echo "${PERSON2}:  am learning devops $PERSON1, Thank you"
