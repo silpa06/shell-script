d5b8287da95ee04a31ce19c4e6e10119578edc47
Silpa <silpa.jr@gmail.com>
2024-08-25 09:23:38 +0800

    new commit

--- variables1.sh
+++ variables1.sh
@@ -2,6 +2,6 @@
 PERSON1="Ramesh"
 PERSON2="Suresh"
 echo "$PERSON1: Hello ${PERSON2}"
-echo "$PERSON2: I am fine Ramesh, how are you?"
+echo "${PERSON2}: I am fine $PERSON1, how are you?"
 echo "$PERSON1: I sm fine ${PERSON2}, what's going on" 
-echo "${PERSON1}:  am learning devops $PERSON2, Thank you"
+echo "${PERSON2}:  am learning devops $PERSON1, Thank you"
